9f05317958ac854a016ec3102420d0a956298fe1
Dhyan Mohandas <dhyanmohandas@gmail.com>
2020-07-23 01:43:03 +0530

    Check box handle

--- App.js
+++ App.js
@@ -11,9 +11,28 @@ class App extends Component{
       this.state = {
          todos : todoData
       }
+      this.handleChange = this.handleChange.bind(this)
    }
+
+   handleChange(id){
+      
+      console.log("Box: " + id)
+
+      this.setState(prevState => {
+            const updateTodos = prevState.todos.map((todo) =>{
+               if(todo.id===id){
+                  todo.completed =  !todo.completed
+               }
+               return todo
+            })
+            return { todos: updateTodos } 
+      })
+
+   }
+
    render(){
-      const todoItems = this.state.todos.map(item => <TodoItem key = {item.id} item = {item} />);
+     
+      const todoItems = this.state.todos.map(item => <TodoItem key = {item.id} item = {item} handleChange = {this.handleChange} />);
       return(
          <div>
            {todoItems}
